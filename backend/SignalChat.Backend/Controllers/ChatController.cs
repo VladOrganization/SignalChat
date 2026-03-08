bda0886fc035a7bfc708ade80e4177d24e178e0b
Kirillfty <kirillhaiduk4@gmail.com>
2026-03-08 22:55:48 +0300

    create a http load-image request
    test this request tomorrow
    and add a signalR request by views this image in chat in real time

--- backend/SignalChat.Backend/Controllers/ChatController.cs
+++ backend/SignalChat.Backend/Controllers/ChatController.cs
@@ -6,6 +6,9 @@ using SignalChat.Backend.Exceptions;
 using SignalChat.Backend.Features.Chat.GetMessages;
 using SignalChat.Backend.Features.Chat.SendMessage;
 using SignalChat.Backend.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
 
 namespace SignalChat.Backend.Controllers;
 
@@ -18,13 +21,40 @@ public class ChatController(ISender sender) : ControllerBase
     public Task<PagedResult<MessageDto>> GetMessages([FromQuery] GetMessagesQuery query, CancellationToken ct)
         => sender.Send(query, ct);
 
+    private readonly IWebHostEnvironment _env;
+    [HttpPost("send-image")]
+    public async Task<IActionResult> SendInage(IFormFile file)
+    {
+        if (file.Length == 0)
+        {
+            return BadRequest("file is not send");
+        }
+        var uploadsFolder = Path.Combine(_env.WebRootPath, "images");
+        Directory.CreateDirectory(uploadsFolder);
+        
+        var fileName = $"{Guid.NewGuid()}.jpg";
+        var filePath = Path.Combine(uploadsFolder, fileName);
+        using (var stream = file.OpenReadStream())
+        using (var image = await Image.LoadAsync(stream))
+        {
+            if (image.Width > 800)
+            {
+                image.Mutate(x => x.Resize(800, 800));
+            }
+
+            var encoder = new JpegEncoder { Quality = 75 };
+            await image.SaveAsync(filePath, encoder);
+        }
+
+        return Ok(new { path = $"/images/{fileName}" });
+    }
     [HttpPost("messages")]
     public Task<MessageDto> SendMessage([FromBody] SendMessageRequest request, CancellationToken ct)
     {
         var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
         if (!Guid.TryParse(userIdStr, out var userId))
             throw new UnauthorizedException("Пользователь не авторизован");
-
+        
         return sender.Send(new SendMessageCommand(userId, request.Text), ct);
     }
 }
