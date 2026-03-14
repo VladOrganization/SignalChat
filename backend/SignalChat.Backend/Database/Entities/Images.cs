6aa9aa530a4bba2d194028c5971c898eb866123b
Kirillfty <kirillhaiduk4@gmail.com>
2026-03-14 14:40:49 +0300

    crate a image entity

---
+++ backend/SignalChat.Backend/Database/Entities/Images.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalChat.Backend.Database.Entities
+{
+    public class Images
+    {
+        public Guid id { get; set; }
+        public string ImageUrl {  get; set; }
+
+        public Guid MessageId { get; set; }
+    }
+}
