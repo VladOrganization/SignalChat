b54f2373e6cac40669416028f101752c5b552a67
Kirillfty <kirillhaiduk4@gmail.com>
2026-03-27 12:27:11 +0300

    update reaction db

--- backend/SignalChat.Backend/Database/Entities/Reaction.cs
+++ backend/SignalChat.Backend/Database/Entities/Reaction.cs
@@ -11,5 +11,7 @@ namespace SignalChat.Backend.Database.Entities
         public Message Message { get; set; }
 
         public Guid MessageId { get; set; }
+        public User User { get; set; }
+        public Guid UserId {  get; set; }
     }
 }
